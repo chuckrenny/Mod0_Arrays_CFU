ad85d7040f76ced4c0be117cecd610b6b1c55a80
Charles Ren <charlesren99@gmail.com>
2023-06-08 17:01:57 -0700

    Call 4 Methods on 4 Arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ strArr = ['a', 'b', 'c', 'd']
 intArr = [1,2,3,4]
 floatArr = [1.0, 2.0, 3.0, 4.0]
 boolArr = [true, true, false, false]
+
+# Removes the last element from strArr 'd'
+p strArr.pop()
+
+# Pushes element to the end of intArr
+p intArr.push(5)
+
+# Removes the first element of floatArr
+p floatArr.shift()
+
+# Adds to first element of boolArr
+p boolArr.unshift(false)
