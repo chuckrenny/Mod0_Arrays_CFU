3fb99ebc5e2f29ebd7ba98e9ef30d02c3faa1530
Charles Ren <charlesren99@gmail.com>
2023-06-08 17:08:29 -0700

    Index Positions and New Method

--- arrays.rb
+++ arrays.rb
@@ -3,6 +3,7 @@ intArr = [1,2,3,4]
 floatArr = [1.0, 2.0, 3.0, 4.0]
 boolArr = [true, true, false, false]
 
+
 # Removes the last element from strArr 'd'
 p strArr.pop()
 
@@ -14,3 +15,14 @@ p floatArr.shift()
 
 # Adds to first element of boolArr and output array
 p boolArr.unshift(false)
+
+
+# Demonstate Understanding of Index Positions
+boolArr[4] = false # as in the previous step we added another element
+boolArr[5] = undefined
+boolArr[-1] = false # as it will wrap around
+
+
+# New Method: Array.new()
+newArr = Array.new(4, 0) 
+# creates an array length 4 populated w the element 0 four times
